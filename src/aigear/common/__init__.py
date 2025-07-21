b7323db85ddb31fb95e820232a6ff4af37896245
ErraticO <10154231wang_yunhua@cn.tre-inc.com>
2025-07-21 16:50:14 +0800

    Add cloud logger

--- src/aigear/common/__init__.py
+++ src/aigear/common/__init__.py
@@ -1,14 +1,5 @@
-from .logger import logger
-from .state import state, StateType
-from . import callable
-from . import hashing
 from .sh import run_sh
 
 __all__ = [
-    "logger",
-    "state",
-    "StateType",
-    "callable",
-    "hashing",
     "run_sh",
 ]
--- src/aigear/common/logger.py
+++ src/aigear/common/logger.py
@@ -1,30 +1,56 @@
 import logging
+import json
 import sys
-# import json
+from google.cloud.logging_v2.handlers import StructuredLogHandler
 
 
-# # TODO: Google Cloud is unable to capture JSON format logs. Can only capture messages
-# class JsonFormatter(logging.Formatter):
-#     def format(self, record):
-#         log_data = {
-#             'process': record.process,
-#             'timestamp': self.formatTime(record),
-#             'level': record.levelname,
-#             'message': record.getMessage(),
-#         }
-#         return json.dumps(log_data)
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_data = {
+            'process': record.process,
+            'timestamp': self.formatTime(record),
+            'level': record.levelname,
+            'message': record.getMessage(),
+        }
+        return json.dumps(log_data)
 
 
-def init_logger():
-    logger_instance = logging.getLogger(__name__)
-    handler = logging.StreamHandler(sys.stdout)
-    # formatter = JsonFormatter()
-    formatter = logging.Formatter('aigear-%(process)d - %(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger_instance.addHandler(handler)
-    logger_instance.setLevel(logging.INFO)
-    return logger_instance
+class Logging:
+    def __init__(
+        self,
+        log_name: str = 'medovik_logging',
+        project_id: str = None
+    ):
+        self.client = None
+        self.project_id = project_id
+        self.log_name = log_name
 
+    def root_logger(self):
+        logger = logging.getLogger(self.log_name)
+        logger.setLevel(logging.INFO)
+        return logger
 
-# Global logger
-logger = init_logger()
+    def gcp_logging_handler(self):
+        handler = StructuredLogHandler(project_id=self.project_id)
+        return handler
+
+    def cloud_logging(self):
+        logger = self.root_logger()
+        console_handler = self.console_logging_handler()
+        gcp_handler = self.gcp_logging_handler()
+        logger.addHandler(console_handler)
+        logger.addHandler(gcp_handler)
+        return logger
+
+    @staticmethod
+    def console_logging_handler():
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = JsonFormatter()
+        handler.setFormatter(formatter)
+        return handler
+
+    def console_logging(self):
+        logger = self.root_logger()
+        console_handler = self.console_logging_handler()
+        logger.addHandler(console_handler)
+        return logger
