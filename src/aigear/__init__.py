ffc1285ef74d956b6fab27493614c1031ce4105d
ErraticO <10154231wang_yunhua@cn.tre-inc.com>
2025-09-24 10:27:04 +0800

    Rm unuse

--- src/aigear/__init__.py
+++ src/aigear/__init__.py
@@ -1,10 +1,7 @@
-import os
 from .project import Project
 from ._version import __version__
 from .common import (
-    run_sh,
     Logging,
-    read_config,
     generate_schema,
     generate_schema_for_json,
     SecretManager,
@@ -12,13 +9,8 @@ from .common import (
 from aigear.infrastructure import Infra
 from aigear.deploy.gcp import Scheduler
 
-aigear_config = read_config(env_path=os.path.join(os.getcwd(), "env.json"))
-aigear_logger = Logging(log_name='aigear_logging').console_logging()
-
 __all__ = [
-    "run_sh",
     "Logging",
-    "aigear_config",
     "generate_schema",
     "generate_schema_for_json",
     "SecretManager",
