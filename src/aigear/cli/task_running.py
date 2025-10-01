1fcf080cc3f96e4417b1e133f96a1dd6df04f247
ErraticO <10154231wang_yunhua@cn.tre-inc.com>
2025-10-01 19:22:29 +0800

    Fix task run cli

--- src/aigear/cli/task_running.py
+++ src/aigear/cli/task_running.py
@@ -1,11 +1,16 @@
 import importlib
 import logging
 import argparse
+import sys
 import os
 
+
 def run_step(pipeline_version, step):
-    os.makedirs(f'output/{pipeline_version}', exist_ok=True)
-    module_name = f"pipelines.{step}"
+    project_root = os.getcwd()
+    if project_root not in sys.path:
+        sys.path.insert(0, project_root)
+
+    module_name = f"src.pipelines.{step}"
     try:
         module = importlib.import_module(module_name)
         module.main(pipeline_version)
@@ -22,7 +27,6 @@ def get_argument():
                         help='Name of the pipeline step')
     args = parser.parse_args()
     return args
-
 
 def task_run():
     args = get_argument()
