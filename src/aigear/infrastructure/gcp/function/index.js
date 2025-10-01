27f68b6eb2cf80d34e6319c801474513b51c8fdd
ErraticO <10154231wang_yunhua@cn.tre-inc.com>
2025-10-01 19:25:43 +0800

    Optimize cloud function

--- src/aigear/infrastructure/gcp/function/index.js
+++ src/aigear/infrastructure/gcp/function/index.js
@@ -8,11 +8,22 @@ const Buffer = require('safe-buffer').Buffer;
 const Compute = require('@google-cloud/compute');
 const compute = new Compute();
 // Change this const value to your project
-const projectId = 'ssc-ape-staging'; ////
-const zone = 'asia-northeast1-a';    ////
+const projectId = 'ssc-ape-staging';
+const zone = 'asia-northeast1-a';
 
 //Build environment and clean up
-const commonCommand = 'cd /var\ngcloud auth configure-docker asia-northeast1-docker.pkg.dev --quiet\nsudo docker pull ${dockerImage}\nsudo docker run ${dockerImage} ${pipelineCommand}\ndocker_exit_code=$?\n[ ${docker_exit_code} -eq "0" ] && gcloud pubsub topics publish medovik-pipelines-pubsub --message \'createVMDate\' || gcloud pubsub topics publish medovik-pipelines-pubsub --message "Exit code: ${docker_exit_code}"\ngcp_zone=$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)\nsleep 300\nhostname_result=$(hostname)\nextracted_name=$(echo ${hostname_result} | cut -d. -f1)\ngcloud compute instances delete ${extracted_name} --zone ${gcp_zone}';
+const commonCommand =
+  'cd /var\n' +
+  'gcloud auth configure-docker asia-northeast1-docker.pkg.dev --quiet\n' +
+  'sudo docker pull ${dockerImage}\n' +
+  'sudo docker run --gpus all ${dockerImage} ${pipelineCommand}\n' +
+  'docker_exit_code=$?\n' +
+  "[ ${docker_exit_code} -eq \"0\" ] && gcloud pubsub topics publish medovik-pipelines-pubsub --message 'createVMDate' || gcloud pubsub topics publish medovik-pipelines-pubsub --message \"Exit code: ${docker_exit_code}\"\n" +
+  'gcp_zone=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)\n' +
+  'sleep 300\n' +
+  'hostname_result=$(hostname)\n' +
+  'extracted_name=$(echo ${hostname_result} | cut -d. -f1)\n' +
+  'gcloud compute instances delete ${extracted_name} --zone ${gcp_zone}';
 
 const vmConfig = {
   kind: 'compute#instance',
@@ -21,7 +32,7 @@ const vmConfig = {
   machineType: `projects/${projectId}/zones/${zone}/machineTypes/`,
   displayDevice: {
     enableDisplay: false
-  },  
+  },
   metadata: {
     kind: 'compute#metadata',
     items: [
@@ -107,33 +118,64 @@ functions.cloudEvent('cronjobProcessPubSub', cloudEvent => {
     return;
   }
   console.log(`vmName is ${cronjobInfo[0].vm_name}`);
-  console.log(`command is ${cronjobInfo[0].command}`);
   console.log(`cronjobInfo.spec is ${cronjobInfo[0].spec}`);
 
-  // set vm spec
-  const spec = cronjobInfo[0].spec ? cronjobInfo[0].spec : 'e2-medium';
-  const machineTypeSpec = `projects/${projectId}/zones/${zone}/machineTypes/` + spec;
-  console.log(`machineTypeSpec is ${machineTypeSpec}`);
+  let updateCommand = commonCommand
+  const isGpu = cronjobInfo[0].on_host_maintenance === 'TERMINATE';
+  console.log(`onHostMaintenance is ${cronjobInfo[0].on_host_maintenance}`);
+  if (isGpu) {
+    const spec = (cronjobInfo[0].spec ? String(cronjobInfo[0].spec) : 'n1-standard-4');
+    vmConfig.machineType = `projects/${projectId}/zones/${zone}/machineTypes/` + spec;
+    console.log(`GPU mode detected: e2-* not supported, fallback machine type -> ${spec}`);
 
-  vmConfig.machineType = machineTypeSpec;
+    // T4
+    const T4_ACCELERATOR = `projects/${projectId}/zones/${zone}/acceleratorTypes/nvidia-tesla-t4`;
+    const gpuCount = parseInt(cronjobInfo[0].gpu_count || '1', 10) || 1;
+    vmConfig.guestAccelerators = [
+      { acceleratorType: T4_ACCELERATOR, acceleratorCount: gpuCount }
+    ];
 
-  // VM and hard disk name
-  vmConfig.name = cronjobInfo[0].vm_name;
-  vmConfig.disks[0].deviceName = cronjobInfo[0].vm_name;
-  const diskSizeGb = cronjobInfo[0].disk_size_gb ? cronjobInfo[0].disk_size_gb : '20';
-  vmConfig.disks[0].initializeParams.diskSizeGb = diskSizeGb;
+    // vm image
+    const vmImage = cronjobInfo[0].vm_image ? cronjobInfo[0].vm_image : 'ml-training-gpu-image';
+    vmConfig.disks[0].initializeParams.sourceImage = `projects/${projectId}/global/images/` + vmImage;
+
+    // VM and hard disk name
+    vmConfig.name = cronjobInfo[0].vm_name;
+    vmConfig.disks[0].deviceName = cronjobInfo[0].vm_name;
+    const diskSizeGb = cronjobInfo[0].disk_size_gb ? cronjobInfo[0].disk_size_gb : '50';
+    vmConfig.disks[0].initializeParams.diskSizeGb = String(Math.max(parseInt(diskSizeGb, 10) || 50, 50));
+  } else {
+    // set vm spec
+    const spec = cronjobInfo[0].spec ? cronjobInfo[0].spec : 'e2-medium';
+    vmConfig.machineType = `projects/${projectId}/zones/${zone}/machineTypes/` + spec;
+
+    // Not T4
+    vmConfig.guestAccelerators = [];
+
+    // set vm image
+    const vmImage = cronjobInfo[0].vm_image ? cronjobInfo[0].vm_image : 'ml-model-training-cloud-function-image';
+    vmConfig.disks[0].initializeParams.sourceImage = `projects/${projectId}/global/images/` + vmImage;
+
+    // VM and hard disk name
+    vmConfig.name = cronjobInfo[0].vm_name;
+    vmConfig.disks[0].deviceName = cronjobInfo[0].vm_name;
+    const diskSizeGb = cronjobInfo[0].disk_size_gb ? cronjobInfo[0].disk_size_gb : '20';
+    vmConfig.disks[0].initializeParams.diskSizeGb = diskSizeGb;
+
+    // cpu rm gpu command
+    updateCommand = updateCommand.replace('--gpus all ', '');
+  }
   vmConfig.scheduling.onHostMaintenance = cronjobInfo[0].on_host_maintenance;
   const vmName = cronjobInfo[0].vm_name + Date.now();
   const dockerImage = cronjobInfo[0].docker_image;
-
   // pipeline step in commonCommand
   const pipelineCommand = '/opt/venv/medovik/bin/python /medovik/main.py --version ' + cronjobInfo[0].pipeline_version + ' --step ' + cronjobInfo[0].pipeline_step
 
   console.log(JSON.stringify(cronjobInfo));
   cronjobInfo.shift()
 
-  vmConfig.metadata.items[0].value = commonCommand.replace(/\${dockerImage}/g,dockerImage).replace(/\${pipelineCommand}/g,pipelineCommand).replace('createVMDate',JSON.stringify(cronjobInfo));
-  
+  vmConfig.metadata.items[0].value = updateCommand.replace(/\${dockerImage}/g,dockerImage).replace(/\${pipelineCommand}/g,pipelineCommand).replace('createVMDate',JSON.stringify(cronjobInfo));
+
   try {
     compute.zone(zone)
       .createVM(vmName, vmConfig)
@@ -152,7 +194,7 @@ functions.cloudEvent('cronjobProcessPubSub', cloudEvent => {
       .catch(err => {
         console.log(err);
       });
-    
+
   } catch (err) {
     console.log(err);
   }
