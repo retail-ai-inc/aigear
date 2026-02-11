a8d518f13a51dc847ae804cf14a49f017b828e7c
10154231wang_yunhua <10154231wang_yunhua@cn.tre-inc.com>
2026-02-11 08:23:08 +0800

    Optimize artifacts

--- src/aigear/cli/artifacts_image.py
+++ src/aigear/cli/artifacts_image.py
@@ -8,9 +8,17 @@ def get_argument():
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--dockerfile_path', default=".",
                         help='path of dockerfile')
+    parser.add_argument('--image_name',
+                        help='The name of the Docker image')
+    parser.add_argument('--image_version', default="latest",
+                        help='The version of the Docker image')
     args = parser.parse_args()
     return args
 
 def artifacts_image_init():
     args = get_argument()
-    create_artifacts_image(dockerfile_path=args.dockerfile_path)
+    create_artifacts_image(
+        dockerfile_path=args.dockerfile_path,
+        image_name=args.image_name,
+        image_version = args.image_version
+    )
