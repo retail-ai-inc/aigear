5d90592b2ffedaafe4df8678a8dbe26a80407eb4
10154231wang_yunhua <10154231wang_yunhua@cn.tre-inc.com>
2026-02-11 08:27:58 +0800

    Add grpc cli

---
+++ src/aigear/cli/grpc_service.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import argparse
+from aigear.service.grpc.grpc_service import grpc_service
+
+
+def get_argument():
+    # Arg
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--model_class_path',
+                        help='Deploy gRPC services by company code')
+    args = parser.parse_args()
+    return args
+
+def grpc_run():
+    project_root = os.getcwd()
+    if project_root not in sys.path:
+        sys.path.insert(0, project_root)
+
+    args = get_argument()
+    grpc_service(args.model_class_path)
