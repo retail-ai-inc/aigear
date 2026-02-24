733887546f212c07afe8991aa79e3c2723b17457
WangYunHua-TREC <10154231wang_yunhua@cn.tre-inc.com>
2026-02-24 10:45:34 +0800

    Simplify the code for artifacts and schedulers

--- src/aigear/cli/artifacts_image.py
+++ src/aigear/cli/artifacts_image.py
@@ -2,18 +2,22 @@ import argparse
 from aigear.deploy.gcp.artifacts_image import create_artifacts_image
 
 
-def get_argument():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+def add_artifacts_args(parser: argparse.ArgumentParser):
     parser.add_argument('--dockerfile_path', default=".",
                         help='path of dockerfile')
     parser.add_argument('--force', action='store_true',
                         help='force recreate image even if it already exists')
-    parser.add_argument('--image_name',
+    parser.add_argument('--image_name', default=None,
                         help='The name of the Docker image')
     parser.add_argument('--image_version', default="latest",
                         help='The version of the Docker image')
+    return parser
+
+def get_argument():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = add_artifacts_args(parser)
     args = parser.parse_args()
     return args
 
--- src/aigear/cli/scheduler.py
+++ src/aigear/cli/scheduler.py
@@ -1,20 +1,22 @@
 import argparse
 from aigear.deploy.gcp.scheduler import create_scheduler
 from aigear.deploy.gcp.artifacts_image import create_artifacts_image
+from aigear.cli.artifacts_image import add_artifacts_args
 
+
+def add_scheduler_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--version', default="",
+                        help='Version of the pipeline')
+    parser.add_argument('--step_names', default="",
+                        help='Name of the pipeline step')
+    return parser
 
 def get_argument():
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--version', default="",
-                        help='Version of the pipeline')
-    parser.add_argument('--step_names', default="",
-                        help='Name of the pipeline step')
-    parser.add_argument('--dockerfile_path', default=".",
-                        help='Path of dockerfile')
-    parser.add_argument('--force', action='store_true',
-                        help='Force recreate image even if it already exists')
+    parser = add_scheduler_args(parser)
+    parser = add_artifacts_args(parser)
     args = parser.parse_args()
     return args
 
@@ -23,6 +25,11 @@ def scheduler_init():
     step_names = args.step_names.split(",")
     
     # Create artifacts image before creating scheduler
-    create_artifacts_image(dockerfile_path=args.dockerfile_path, force=args.force)
+    create_artifacts_image(
+        dockerfile_path=args.dockerfile_path,
+        force=args.force,
+        image_name=args.image_name,
+        image_version = args.image_version
+    )
     
     create_scheduler(args.version, step_names)
