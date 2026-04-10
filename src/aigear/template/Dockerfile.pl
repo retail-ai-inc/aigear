93ace6f71a931a9ac52d8e4207fde614edfe82bb
WangYunHua-TREC <10154231wang_yunhua@cn.tre-inc.com>
2026-04-10 15:49:14 +0800

    Optimize Dockerfile

--- src/aigear/template/Dockerfile.pl
+++ src/aigear/template/Dockerfile.pl
@@ -1,16 +1,20 @@
 FROM python:3.12-slim-bookworm
 COPY --from=ghcr.io/astral-sh/uv:0.5.14 /uv /uvx /bin/
 
+ENV UV_COMPILE_BYTECODE=1 \
+    UV_LINK_MODE=copy \
+    VIRTUAL_ENV=/opt/venv/pl \
+    PATH="/opt/venv/pl/bin:$PATH"
+
 WORKDIR /pl
+
+COPY requirements_pl.txt aigear-0.1.0-py3-none-any.whl ./
+RUN uv venv /opt/venv/pl --python 3.12.7 \
+ && uv pip install --no-cache -r requirements_pl.txt \
+ && uv pip install --no-cache aigear-0.1.0-py3-none-any.whl
+
 COPY . .
 
-RUN uv python install 3.12.7
-
-RUN uv venv /opt/venv/pl --python 3.12.7
-RUN . /opt/venv/pl/bin/activate && uv pip install -r requirements_pl.txt
-
-ENV VIRTUAL_ENV=/opt/venv/pl
-ENV PATH="/opt/venv/pl/bin:$PATH"
 ENV PORT=50051 \
     PYTHONDONTWRITEBYTECODE=1 \
-    PYTHONBUFFERED=1
+    PYTHONUNBUFFERED=1
