0a4e51d1339b19042d95aaad00e64a9ef1b028be
WangYunHua-TREC <10154231wang_yunhua@cn.tre-inc.com>
2026-04-13 09:52:53 +0800

    Optimize the speed of image construction

--- src/aigear/template/Dockerfile.pl
+++ src/aigear/template/Dockerfile.pl
@@ -1,17 +1,15 @@
 FROM python:3.12-slim-bookworm
 COPY --from=ghcr.io/astral-sh/uv:0.5.14 /uv /uvx /bin/
 
-ENV UV_COMPILE_BYTECODE=1 \
-    UV_LINK_MODE=copy \
-    VIRTUAL_ENV=/opt/venv/pl \
-    PATH="/opt/venv/pl/bin:$PATH"
+ENV VENV_PL=/opt/venv/pl
 
 WORKDIR /pl
 
 COPY requirements_pl.txt aigear-0.1.0-py3-none-any.whl ./
-RUN uv venv /opt/venv/pl --python 3.12.7 \
- && uv pip install --no-cache -r requirements_pl.txt \
- && uv pip install --no-cache aigear-0.1.0-py3-none-any.whl
+
+RUN --mount=type=cache,target=/root/.cache/uv \
+    uv venv ${VENV_PL} --python 3.12.7 \
+ && uv pip install --python ${VENV_PL} -r requirements_pl.txt aigear-0.1.0-py3-none-any.whl
 
 COPY . .
 
